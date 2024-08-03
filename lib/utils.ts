ad3c7e0290e16e9fc99260b56e303df7737f22be
Sadam Hussain <d_shussain@universalweather.net>
2024-08-02 21:59:24 -0400

    Create New Document

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,67 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { UserType } from "@/types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export const parseStringify = (value: any) => JSON.parse(JSON.stringify(value));
+
+export const getAccessType = (userType: UserType) => {
+  switch (userType) {
+    case "creator":
+      return ["room:write"];
+    case "editor":
+      return ["room:write"];
+    case "viewer":
+      return ["room:read", "room:presence:write"];
+    default:
+      return ["room:read", "room:presence:write"];
+  }
+};
+
+export const dateConverter = (timestamp: string): string => {
+  const timestampNum = Math.round(new Date(timestamp).getTime() / 1000);
+  const date: Date = new Date(timestampNum * 1000);
+  const now: Date = new Date();
+
+  const diff: number = now.getTime() - date.getTime();
+  const diffInSeconds: number = diff / 1000;
+  const diffInMinutes: number = diffInSeconds / 60;
+  const diffInHours: number = diffInMinutes / 60;
+  const diffInDays: number = diffInHours / 24;
+
+  switch (true) {
+    case diffInDays > 7:
+      return `${Math.floor(diffInDays / 7)} weeks ago`;
+    case diffInDays >= 1 && diffInDays <= 7:
+      return `${Math.floor(diffInDays)} days ago`;
+    case diffInHours >= 1:
+      return `${Math.floor(diffInHours)} hours ago`;
+    case diffInMinutes >= 1:
+      return `${Math.floor(diffInMinutes)} minutes ago`;
+    default:
+      return "Just now";
+  }
+};
+
+// Function to generate a random color in hex format, excluding specified colors
+export function getRandomColor() {
+  const avoidColors = ["#000000", "#FFFFFF", "#8B4513"]; // Black, White, Brown in hex format
+
+  let randomColor;
+  do {
+    // Generate random RGB values
+    const r = Math.floor(Math.random() * 256); // Random number between 0-255
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    // Convert RGB to hex format
+    randomColor = `#${r.toString(16)}${g.toString(16)}${b.toString(16)}`;
+  } while (avoidColors.includes(randomColor));
+
+  return randomColor;
 }
 
 export const brightColors = [
--- types/index.ts
+++ types/index.ts
@@ -1,13 +1,15 @@
 // /* eslint-disable no-unused-vars */
-// declare type SearchParamProps = {
-//     params: { [key: string]: string };
-//     searchParams: { [key: string]: string | string[] | undefined };
-// };
-//
-// declare type AccessType = ["room:write"] | ["room:read", "room:presence:write"];
-//
-// declare type RoomAccesses = Record<string, AccessType>;
-//
+import { BaseMetadata, ThreadData } from "@liveblocks/core";
+
+export type SearchParamProps = {
+  params: { [key: string]: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export type AccessType = ["room:write"] | ["room:read", "room:presence:write"];
+
+export type RoomAccesses = Record<string, AccessType>;
+
 export type UserType = "creator" | "editor" | "viewer";
 
 export type RoomMetadata = {
@@ -29,52 +31,52 @@ export type User = {
   color: string;
   userType?: UserType;
 };
-//
-// declare type ShareDocumentParams = {
-//     roomId: string;
-//     email: string;
-//     userType: UserType;
-//     updatedBy: User;
-// };
-//
-// declare type UserTypeSelectorParams = {
-//     userType: string;
-//     setUserType: React.Dispatch<React.SetStateAction<UserType>>;
-//     onClickHandler?: (value: string) => void;
-// };
-//
-// declare type ShareDocumentDialogProps = {
-//     roomId: string;
-//     collaborators: User[];
-//     creatorId: string;
-//     currentUserType: UserType;
-// };
-//
-// declare type HeaderProps = {
-//     children: React.ReactNode;
-//     className?: string;
-// };
-//
-// declare type CollaboratorProps = {
-//     roomId: string;
-//     email: string;
-//     creatorId: string;
-//     collaborator: User;
-//     user: User;
-// };
-//
-// declare type CollaborativeRoomProps = {
-//     roomId: string;
-//     roomMetadata: RoomMetadata;
-//     users: User[];
-//     currentUserType: UserType;
-// };
-//
-// declare type AddDocumentBtnProps = {
-//     userId: string;
-//     email: string;
-// };
-//
-// declare type DeleteModalProps = { roomId: string };
-//
-// declare type ThreadWrapperProps = { thread: ThreadData<BaseMetadata> };
+
+export type ShareDocumentParams = {
+  roomId: string;
+  email: string;
+  userType: UserType;
+  updatedBy: User;
+};
+
+export type UserTypeSelectorParams = {
+  userType: string;
+  setUserType: React.Dispatch<React.SetStateAction<UserType>>;
+  onClickHandler?: (value: string) => void;
+};
+
+export type ShareDocumentDialogProps = {
+  roomId: string;
+  collaborators: User[];
+  creatorId: string;
+  currentUserType: UserType;
+};
+
+export type HeaderProps = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+export type CollaboratorProps = {
+  roomId: string;
+  email: string;
+  creatorId: string;
+  collaborator: User;
+  user: User;
+};
+
+export type CollaborativeRoomProps = {
+  roomId: string;
+  roomMetadata: RoomMetadata;
+  users: User[];
+  currentUserType: UserType;
+};
+
+export type AddDocumentBtnProps = {
+  userId: string;
+  email: string;
+};
+
+export type DeleteModalProps = { roomId: string };
+
+export type ThreadWrapperProps = { thread: ThreadData<BaseMetadata> };
