d97bcc4a742d39cdedf2b37539c2999c8874de83
Sadam Hussain <d_shussain@universalweather.net>
2024-08-03 19:27:05 -0400

    Integrate Sentry

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -2,7 +2,6 @@
 import React from "react";
 import {
   LiveblocksProvider,
-  RoomProvider,
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
 import Loader from "@/components/ui/loader";
@@ -38,9 +37,7 @@ const Provider: React.FC<ProviderProps> = ({ children }) => {
         return roomUsers;
       }}
     >
-      {/*<RoomProvider id="my-room">*/}
       <ClientSideSuspense fallback={<Loader />}>{children}</ClientSideSuspense>
-      {/*</RoomProvider>*/}
     </LiveblocksProvider>
   );
 };
