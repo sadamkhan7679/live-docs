5d309f5b9c057d0935ef2b91ce44c8ba0d1045ec
Sadam Hussain <d_shussain@universalweather.net>
2024-08-02 20:35:41 -0400

    Add Lexical Editor Package

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Editor} from "@/components/modules/editor/Editor";
+
+type DocumentPageProps = {};
+
+
+const DocumentPage:React.FC<DocumentPageProps> = () => {
+	return (
+		<Editor />
+	)
+}
+
+export default DocumentPage
